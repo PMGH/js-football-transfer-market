9f841b4fecaf91f0b494aaff97e44f9c9814b2ee
Peter McCready <petermccready2@gmail.com>
2017-12-12 18:20:47 +0000

    Adds makeRequest and requestComplete functionality

--- client/src/app.js
+++ client/src/app.js
@@ -1,3 +1,23 @@
-window.addEventListener("load", function(){
-	alert("Loaded!");
-});
+var makeRequest = function(url, callback){
+	var request = new XMLHttpRequest();
+	request.open("GET", url);
+	request.addEventListener(url, callback);
+	request.send();
+};
+
+var requestComplete = function(){
+	if (this.status !== 200){
+		return console.log("Request failed");
+	}
+	console.log("Request successful");
+	var jsonString = this.responseText;
+	var apiData = JSON.parse(jsonString);
+	console.log(apiData);
+};
+
+var app = function(){
+	var url = "/players";
+	makeRequest(url, requestComplete);
+};
+
+window.addEventListener("load", app);
